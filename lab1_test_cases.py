b694fe8258881c502df29fe7935759f5c4ea3492
ameliabruscia <aibrusci@calpoly.edu>
2019-04-08 18:22:48 -0700

    lab1 test cases

--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -29,11 +29,11 @@ class TestLab1(unittest.TestCase):
         list_val =[0,1,2,3,4,7,8,9,10]
         low = 0
         high = len(list_val)-1
-        self.assertEqual(bin_search(4, low, high, list_val), 4 )
+        self.assertEqual(bin_search(4, low, high, list_val), 4)
         list_val2 = [0,1,2,3,4,7,8,9,10] 
         low = 2
         high = 8
-        self.assertEqual(bin_search(1, low, high, list_val2), None )
+        self.assertEqual(bin_search(1, low, high, list_val2), None)
         list_val3 = [1,2,3,4,7]
         low = 0
         high = 4
@@ -42,13 +42,13 @@ class TestLab1(unittest.TestCase):
         with self.assertRaises(ValueError):  # used to check for exception
            bin_search(1, 0, 4, list_val4)
         list_val5 = [1]
-        self.assertEqual(bin_search(1, 0, 1, list_val5), 0 )
+        self.assertEqual(bin_search(1, 0, 1, list_val5), 0)
         list_val6 = [1,2,3,4]
-        self.assertEqual(bin_search(2, 0, 3, list_val6), 1 ) 
+        self.assertEqual(bin_search(2, 0, 3, list_val6), 1)
         list_val7 = [1,2,3,4]
-        self.assertEqual(bin_search(3, 0, 3, list_val7), 2 )
+        self.assertEqual(bin_search(3, 0, 3, list_val7), 2)
         list_val7 = [1,2,3,4]
-        self.assertEqual(bin_search(7, 0, 3, list_val7), None )
+        self.assertEqual(bin_search(7, 0, 3, list_val7), None)
  
 if __name__ == "__main__":
         unittest.main()
